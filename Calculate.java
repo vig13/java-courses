0472e3337515e70bf032d54294c87077a592b6a3
vig13 <viguro13@gmail.com>
2017-01-22 21:58:02 +0500

    Calculate finish

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,30 @@
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Calculate");
+		char operations = args[1].charAt(0);;
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[2]);
+
+	if(operations == '+' || operations == '-' || operations == '/' || operations == '^' || operations == 'p'){
+		switch(operations){
+		case '+':
+		System.out.println(first + second);
+		break;
+		case '-':
+		System.out.println(first - second);
+		break;
+		case 'p':
+		System.out.println(first * second);
+		break;
+		case '/':
+		System.out.println(first / second);
+		break;
+		case '^':
+		System.out.println(Math.pow(first, second));
+		break;
+		}
+	}else{
+		System.out.println("Введите верное выражение!");
+	}
 	}
 }
